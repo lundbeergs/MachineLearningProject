16ef6b69244903df853bfbaaf6b93b1afaa7b92f
Calle1289 <carl.edling@live.sem>
2023-09-27 16:22:35 +0200

    Gjorde en ny branch med min lösning till Part 1

--- Lab_1.py
+++ Lab_1.py
@@ -1,15 +1,52 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 
+# Load in the dataset
 x_test_regression = np.load('X_test_regression1.npy')
 x_train_regression = np.load('X_train_regression1.npy')
 y_train_regression = np.load('y_train_regression1.npy')
 
-model_linear_regression = LinearRegression()
+# Determent the amount of folds
+k = 5
 
-model_linear_regression.fit(x_train_regression, y_train_regression)
+# Creat kfolds
+kf = KFold(n_splits=k, shuffle=True, random_state=39)
 
-y_pred = model_linear_regression.predict(x_test_regression)
+# Create models
+model_Linear_regression = LinearRegression()
+model_Ridge = Ridge()
+mode_Lasso = Lasso()
 
-np.save('y_pred', y_pred)
+# Make models into interable list
+list_models = [model_Linear_regression, model_Ridge, mode_Lasso]
+
+# Save model scores
+model_mean_mse_scores = []
+
+# Iterate over models
+for model in list_models:
+
+    # Save fold scores
+    model_mse_scores = []
+
+    # Split the training set into test and training sets
+    for index_train, index_test in kf.split(x_train_regression):
+        x_train_fold, x_test_fold = x_train_regression[index_train], x_train_regression[index_test]
+        y_train_fold, y_test_fold = y_train_regression[index_train], y_train_regression[index_test]
+
+        # Fit the model to the training data
+        model.fit(x_train_fold, y_train_fold)
+
+        # Predict the results from the model on the fold test data
+        y_pred_fold = model.predict(x_test_fold)
+
+        # Calculate the mean squared error for each fold
+        mse_fold = mean_squared_error(y_test_fold, y_pred_fold)
+        model_mse_scores.append(mse_fold)
+
+    mean_mse_fold = sum(model_mse_scores) / k
+    model_mean_mse_scores.append(mean_mse_fold)
